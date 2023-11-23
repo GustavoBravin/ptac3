a9db5e973d776b9cb2bb1036e18d416ac286fd2f
GustavoBravin <gugarbravin@gmail.com>
2023-11-23 16:47:31 -0400

    que loco 23/11/2023
    que negocio louco

--- app.js
+++ app.js
@@ -7,20 +7,27 @@ app.use(express.static('public'));
 
 const produtos = [
   {id:1 , nome:"Bola",preço: 10, imagem:"bola.png"},
-  {id:2 , nome:"Bola amarela",preço: 10, imagem:"bola amarela.png"},
-  {id:3 , nome:"Bola verde",preço: 10, imagem:"bola verde.jpg"},
-  {id:4 , nome:"Bola azul",preço: 10, imagem:"bola azul.jpg"},
-  {id:5 , nome:"Bola rosa",preço: 10, imagem:"bola rosa.jpg"},
-  {id:6 , nome:"Bola branca",preço: 10, imagem:"bola branca.png"},
-  {id:7 , nome:"Bola preta",preço: 10, imagem:"bola preta.jpg"},
-  {id:8 , nome:"Bola marrom",preço: 10, imagem:"bola marrom.jpg"},
-  {id:9 , nome:"Bola nike",preço: 10, imagem:"bola nike.jpg"},
-  {id:10 , nome:"Bola adidas",preço: 10, imagem:"adidas.jpg"},
+  {id:2 , nome:"Bola amarela",preço: 15, imagem:"bola amarela.png"},
+  {id:3 , nome:"Bola verde",preço: 11, imagem:"bola verde.jpg"},
+  {id:4 , nome:"Bola azul",preço: 12, imagem:"bola azul.jpg"},
+  {id:5 , nome:"Bola rosa",preço: 13, imagem:"bola rosa.jpg"},
+  {id:6 , nome:"Bola branca",preço: 14, imagem:"bola branca.png"},
+  {id:7 , nome:"Bola preta",preço: 16, imagem:"bola preta.jpg"},
+  {id:8 , nome:"Bola marrom",preço: 17, imagem:"bola marrom.jpg"},
+  {id:9 , nome:"Bola nike",preço: 18, imagem:"bola nike.jpg"},
+  {id:10 , nome:"Bola adidas",preço: 19, imagem:"adidas.jpg"},
   
 ]
 
+
+function buscarProdutoPorId(id) {
+  const produto = produtos.find(produto => produto.id = id) ;
+  return produto || null
+}
+
+
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Lojinha ' });
+  res.render('index', { produtos });
 });
 app.get('/produto', (req, res) => {
   res.render('produto', { message: '' });
